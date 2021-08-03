d48e6c271af3bf8cbd4e02a183155d00bedcf4ec
Sarang PM <sarang.pm@cabotsolutions.com>
2021-08-03 12:29:15 +0530

    fixing tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import {useState} from "react";
 function App() {
-  return <div>
-    <div>fdjsghfjhgf
-      <div>
-
-
-
-jhgfhgg
-        hjgfjhgf <p>ashjgdghadhg</p><div>jkhgjg </div>
-        <p>this is a test paragraph</p> 
-      </div>
-    </div>
-  </div>;
+  return <div></div>;
 }
 
 export default App;
