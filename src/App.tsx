2a9f2182cd85c69a98e4d6ba1ee0bec2982f940d
Sarang PM <sarang.pm@cabotsolutions.com>
2021-08-03 13:37:34 +0530

    Adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { LoginPage } from "pages/login";
+import { PostsPage } from "pages/posts";
 function App() {
-  return <div>Sample</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/posts">
+          <PostsPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
